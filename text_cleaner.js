bec047587c1ab7cd98d72def42174ae4d5597703
Pratyush Vashisht <pratyushvashisht1@gmail.com>
2020-09-05 08:51:33 +0200

    First cleaning of text
    Removed characters, numbers, newlines, extra spaces, and question marks

--- text_cleaner.js
+++ text_cleaner.js
@@ -1,3 +1,6 @@
+// Creating a text cleaner for a speech. Later will use this
+// to analyse the topic of the speech with a word cloud
+
 const fs = require('fs');
 const raw_files = './raw/';
 const clean_1_files = './clean_1/';
@@ -5,15 +8,58 @@ const clean_2_files = './clean_2/';
 const clean_3_files = './clean_3/';
 const clean_4_files = './clean_4/';
 
+// testing with 1 file first
+// let filename = '20200830.txt';
+// let filecontents = fs.readFileSync(clean_1_files + filename, 'utf8')
+
+const clean_text = text => {
+  text = text.toLowerCase();
+
+  // remove new-line
+  text = text.replace(/\n/g, " ");
+
+  // remove most common characters, and numbers
+  text = text.replace(/‘|’|\/|\d+|"|“|”|,|;|–|…|-|‘|’|'|!|#|%|&|\*|\(|\)|_|\[|\]|\{|\}|:|<|>/g, " ");
+
+  // replace all places with multiple "space" with a single space
+  text = text.replace(/\s+/g, " ");
+
+  // add a space before all sentence ends so the last word isn't added in short words
+  text = text.replace(/\.|\?/g, " . ");
+
+  return text;
+}
+
+let remove_common_words = text => {
+
+  let commonwords_hash = {};
+  let safelist  = ["toy", "", ".", "our", "eco", "bow", "fun", "big", "hub", "act"];
+  let blocklist = [];
+
+  // splitting the text on each space
+  text.split(" ").forEach(word => {
+      // all short words except the ones above
+      if (word.length <= 3 && word.length > 0 && safelist.indexOf(word) == -1) {
+          commonwords_hash[word] = commonwords_hash[word] ? commonwords_hash[word] + 1 : 1;
+      }
+  });
+  let commonwords = Object.keys(commonwords_hash);
+
+  // we want to return a word if it's not in the array
+  text = text.split(" ").filter(word => commonwords.indexOf(word) == -1).join(" ");
+
+  return text;
+};
+
 fs.readdirSync(clean_1_files, 'utf8').forEach(filename => {
   let filecontents = fs.readFileSync(clean_1_files + filename, 'utf8')
-  console.log(filename, filecontents);
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n\n\n\n');
+  console.log(1, filename, filecontents.length);
+
+  filecontents = clean_text(filecontents);
+  fs.writeFileSync(clean_2_files + filename, filecontents, 'utf8');
+  console.log(2, filename, filecontents.length);
 });
 
-
-// /*
-//  *
 // text.toLowerCase().replace(/‘|’|\/|\d+|"|“|”|,|;|–|…|-|‘|’|'|!|#|%|&|\*|\(|\)|_|\[|\]|\{|\}|:|<|>/g, " ").replace(/\s+| the /g, " ");
 // 
 // let lines = text.split(/\.|\?/).map(line => {
@@ -40,6 +86,3 @@ fs.readdirSync(clean_1_files, 'utf8').forEach(filename => {
 //         clearInterval(timer);
 //     }
 // }, 2500);
-// 
-// *
-// * /
